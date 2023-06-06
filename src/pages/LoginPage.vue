936eb49a04edbe42e650f26f378bec734b66e150
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-06-06 11:28:20 +0300

    login page with auth

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,11 +1,76 @@
 <template>
-    <q-page class="flex flex-center">Login</q-page>
+    <q-page class="flex flex-center">
+        <q-card>
+            <div class="q-pa-md">
+                <q-form
+                    @submit.prevent="onSubmit"
+                    @reset="null"
+                    class="q-gutter-md form-width"
+                >
+                    <q-input
+                        v-model="email"
+                        label="Your email"
+                        lazy-rules
+                        :rules="[(val) => isEmailValid(val) || 'Invalid email']"
+                        :error="error"
+                        error-message="Invalid credentials"
+                    />
+                    <q-input
+                        v-model="password"
+                        type="password"
+                        label="Your password"
+                        :rules="[(val) => !!val || 'Enter password']"
+                    />
+                    <q-btn
+                        label="Submit"
+                        type="submit"
+                        color="primary"
+                        :loading="loading"
+                    />
+                </q-form>
+            </div>
+        </q-card>
+    </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import { useAuthStore } from "stores/auth";
+const authStore = useAuthStore();
 
 export default defineComponent({
     name: "LoginPage",
+    data() {
+        return {
+            email: "",
+            password: "",
+            loading: false,
+            error: false,
+        };
+    },
+    methods: {
+        async onSubmit() {
+            this.loading = true;
+            const result = await authStore.loginUser(this.email, this.password);
+            this.error = !result;
+            if (result) {
+                this.$router.push("/");
+            }
+            this.loading = false;
+        },
+
+        isEmailValid(email) {
+            const emailPattern =
+                /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/;
+            return emailPattern.test(email);
+        },
+    },
 });
 </script>
+
+<style scoped>
+.form-width {
+    min-width: 300px;
+    max-width: 400px;
+}
+</style>
--- src/stores/auth.js
+++ src/stores/auth.js
@@ -5,6 +5,7 @@ export const useAuthStore = defineStore("auth", {
     state: () => ({
         token: null,
         email: null,
+        username: null,
     }),
     actions: {
         async registerUser(email, password) {
@@ -15,6 +16,22 @@ export const useAuthStore = defineStore("auth", {
             const res = await api
                 .post("/api/user/create/", payload)
                 .then((response) => {
+                    return true;
+                })
+                .catch((error) => {
+                    return false;
+                });
+            return res;
+        },
+        async loginUser(email, password) {
+            const payload = {
+                email: email,
+                password: password,
+            };
+            const res = await api
+                .post("/api/user/token/", payload)
+                .then((response) => {
+                    this.token = response.data.token;
                     return true;
                 })
                 .catch((error) => {
