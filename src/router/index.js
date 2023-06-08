0330e6aba6633d15f30ca00ee7a096747cc6650a
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-06-08 18:57:21 +0300

    auth policy for each router path

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
-import { route } from 'quasar/wrappers'
-import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
-import routes from './routes'
+import { route } from "quasar/wrappers";
+import {
+    createRouter,
+    createMemoryHistory,
+    createWebHistory,
+    createWebHashHistory,
+} from "vue-router";
+import routes from "./routes";
+import { useAuthStore } from "src/stores/auth";
 
 /*
  * If not building with SSR mode, you can
@@ -12,19 +18,39 @@ import routes from './routes'
  */
 
 export default route(function (/* { store, ssrContext } */) {
-  const createHistory = process.env.SERVER
-    ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)
+    const authStore = useAuthStore();
+    const createHistory = process.env.SERVER
+        ? createMemoryHistory
+        : process.env.VUE_ROUTER_MODE === "history"
+        ? createWebHistory
+        : createWebHashHistory;
 
-  const Router = createRouter({
-    scrollBehavior: () => ({ left: 0, top: 0 }),
-    routes,
+    const Router = createRouter({
+        scrollBehavior: () => ({ left: 0, top: 0 }),
+        routes,
 
-    // Leave this as is and make changes in quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    // quasar.conf.js -> build -> publicPath
-    history: createHistory(process.env.VUE_ROUTER_BASE)
-  })
+        // Leave this as is and make changes in quasar.conf.js instead!
+        // quasar.conf.js -> build -> vueRouterMode
+        // quasar.conf.js -> build -> publicPath
+        history: createHistory(process.env.VUE_ROUTER_BASE),
+    });
 
-  return Router
-})
+    Router.beforeEach((to, from, next) => {
+        if (to.meta.unauthenticatedOnly && authStore.isAuthenticated) {
+            return next("/");
+        } else if (to.meta.unauthenticatedOnly && !authStore.isAuthenticated) {
+            return next();
+        } else if (to.meta.authOnly && authStore.isAuthenticated) {
+            return next();
+        } else if (to.meta.authOnly && !authStore.isAuthenticated) {
+            return next("/login");
+        } else if (to.meta.adminOnly && authStore.isStaff) {
+            return next();
+        } else if (to.meta.adminOnly && !authStore.isStaff) {
+            return next("/");
+        }
+        return next();
+    });
+
+    return Router;
+});
--- src/router/routes.js
+++ src/router/routes.js
@@ -3,20 +3,48 @@ const routes = [
         path: "/",
         component: () => import("layouts/MainLayout.vue"),
         children: [
-            { path: "", component: () => import("pages/IndexPage.vue") },
+            {
+                path: "",
+                component: () => import("pages/IndexPage.vue"),
+                meta: {
+                    authOnly: false,
+                    adminOnly: false,
+                    unauthenticatedOnly: false,
+                },
+            },
             {
                 path: "/registration",
                 component: () => import("pages/RegistrationPage.vue"),
+                meta: {
+                    authOnly: false,
+                    adminOnly: false,
+                    unauthenticatedOnly: true,
+                },
             },
-            { path: "/login", component: () => import("pages/LoginPage.vue") },
+            {
+                path: "/login",
+                component: () => import("pages/LoginPage.vue"),
+                meta: {
+                    authOnly: false,
+                    adminOnly: false,
+                    unauthenticatedOnly: true,
+                },
+            },
         ],
+        meta: {
+            authOnly: false,
+            adminOnly: false,
+            unauthenticatedOnly: false,
+        },
     },
-
-    // Always leave this as last one,
-    // but you can also remove it
     {
         path: "/:catchAll(.*)*",
         component: () => import("pages/ErrorNotFound.vue"),
+        meta: {
+            authOnly: false,
+            adminOnly: false,
+            unauthenticatedOnly: false,
+        },
     },
 ];
 
