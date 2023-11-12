8383ca61297d2a88b87c595c22e1d545bb33d5f5
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-11-12 16:46:55 +0200

    feat: achievements page

---
+++ src/components/AchievementCard.vue
@@ -0,0 +1,42 @@
+<template>
+    <q-card class="achievement_card">
+        <q-img :src="getImageToDisplay(achievement)">
+            <div
+                class="absolute-bottom text-subtitle2 text-center achievement_title">
+                {{ achievement.title }}
+            </div>
+        </q-img>
+        <q-card-section class="text-center">
+            {{ achievement.description }}
+        </q-card-section>
+    </q-card>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: "AchievementCard",
+    props: ['achievement'],
+    methods: {
+        getImageToDisplay(achievement) {
+            if (!achievement.done) {
+                return 'src/assets/locked_achievement.png'
+            }
+            return achievement.image ? achievement.image : 'src/assets/no_image_available.png'
+        }
+    }
+})
+</script>
+
+
+<style scoped>
+.achievement_card {
+    width: 100%;
+    max-width: 150px;
+}
+
+.achievement_title {
+    padding: 2px;
+}
+</style>
--- src/pages/AchievementsPage.vue
+++ src/pages/AchievementsPage.vue
@@ -1,14 +1,22 @@
 <template>
-    <div> {{ achievements }} </div>
+    <q-page class="q-pa-md row items-start q-gutter-md justify-center">
+        <AchievementCard v-for="achievement in achievements" :key="achievement.id"
+            :achievement="achievement">
+        </AchievementCard>
+    </q-page>
 </template>
 <script>
 import { defineComponent } from 'vue';
 import { useAchievementsStore } from 'src/stores/achievements';
+import AchievementCard from 'src/components/AchievementCard.vue';
 
 const achievementsStore = useAchievementsStore()
 
 export default defineComponent({
     name: "AchievementsPage",
+    components: {
+        AchievementCard
+    },
     setup() {
         achievementsStore.getAchievements()
     },
