94280e71bc036ea4a0cb4ae476f01bc6f51078ca
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-03-19 18:02:05 +0200

    ref: remove email and registration buttons from header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,44 +1,17 @@
 <template>
     <q-header elevated class="bg-primary text-white">
         <q-toolbar>
-            <q-btn
-                dense
-                flat
-                round
-                icon="menu"
-                @click="$emit('toggleLeftDrawer')"
-            />
-            <q-space></q-space>
-            <div class="flex">
-                <div v-if="!isAuthenticated">
-                    <q-btn flat to="/registration">Register</q-btn>
-                    <q-btn flat to="/login">Login</q-btn>
-                </div>
-                <div v-else>
-                    <q-toolbar-title>{{ displayName }}</q-toolbar-title>
-                </div>
-            </div>
+            <q-btn dense flat round icon="menu"
+                @click="$emit('toggleLeftDrawer')" />
         </q-toolbar>
     </q-header>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-import { useAuthStore } from "stores/auth";
-
-const authStore = useAuthStore();
 
 export default defineComponent({
     name: "TheHeader",
     emits: ["toggleLeftDrawer"],
-    computed: {
-        displayName() {
-            return authStore.email;
-        },
-        isAuthenticated() {
-            return authStore.isAuthenticated;
-        },
-    },
-    methods: {},
 });
 </script>
