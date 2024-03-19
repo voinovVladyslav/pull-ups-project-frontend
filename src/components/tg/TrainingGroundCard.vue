3ca3abb89fbbc24db575d6d7807a8a8382088a4e
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-03-19 16:42:20 +0200

    feat: training ground card

--- src/components/tg/TrainingGroundCard.vue
+++ src/components/tg/TrainingGroundCard.vue
@@ -1,7 +1,6 @@
 <template>
-    <q-card class="row q-ma-md justify-between"
-        :class="{ 'favorite': tg.is_favorite }">
-        <q-card-section class="col-2 q-pa-sm">Training Ground #{{ tg.id
+    <q-card class="row q-ma-md justify-between">
+        <q-card-section class="col-2 q-pa-sm">#{{ tg.id
             }}</q-card-section>
         <q-card-section class="col-2 q-pa-sm">Has pullup bar: {{ !!tg.pullupbar
             }}</q-card-section>
@@ -10,20 +9,42 @@
         }}</q-card-section>
         <q-card-section class="col-2 q-pa-sm">Add to favorite: {{ tg.is_favorite
             }}</q-card-section>
-        <q-card-section class="col-2 q-pa-sm">location: to be</q-card-section>
+        <q-card-section v-if="location" class="col-2 q-pa-sm">{{
+            roundToTwoDigits(distance(location, tg.location.coordinates))
+        }} km</q-card-section>
+        <q-card-section v-else>
+            <q-spinner size="sm" />
+        </q-card-section>
+        <q-card-actions>
+            <q-icon>
+
+            </q-icon>
+        </q-card-actions>
     </q-card>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
+import { distance, roundToTwoDigits } from 'src/utils/coordinates';
+import { useGeolocationStore } from 'src/stores/geolocation';
+
+const geolocationStore = useGeolocationStore();
+
 export default defineComponent({
     name: 'TrainingGroundCard',
     props: ['tg'],
+    setup() {
+        return {
+            distance,
+            roundToTwoDigits,
+        };
+    },
+    computed: {
+        location() {
+            return geolocationStore.location;
+        },
+    },
 })
 </script>
 
-<style scoped>
-.favorite {
-    border: 2px solid pink;
-}
-</style>
+<style scoped></style>
---
+++ src/utils/coordinates.js
@@ -0,0 +1,36 @@
+function distance(currentLocation, coordinates) {
+    const lat1 = currentLocation.latitude;
+    const lon1 = currentLocation.longitude;
+    const lat2 = coordinates[1];
+    const lon2 = coordinates[0];
+
+    if (lat1 == lat2 && lon1 == lon2) {
+        return 0;
+    }
+    let radlat1 = (Math.PI * lat1) / 180;
+    let radlat2 = (Math.PI * lat2) / 180;
+    let theta = lon1 - lon2;
+    let radtheta = (Math.PI * theta) / 180;
+    let dist =
+        Math.sin(radlat1) * Math.sin(radlat2) +
+        Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    if (dist > 1) {
+        dist = 1;
+    }
+    dist = Math.acos(dist);
+    dist = (dist * 180) / Math.PI;
+    dist = dist * 60 * 1.1515;
+    dist = dist * 1.609344;
+    return dist;
+}
+function roundToTwoDigits(number) {
+    return Math.round((number + Number.EPSILON) * 100) / 100;
+}
+function createLinkToGoogleMaps(destinationPoint, location) {
+    const toPoint = `${destinationPoint[1]},${destinationPoint[0]}`;
+    const fromPoint = `${location.latitude},${location.longitude}`;
+    const url = `https://www.google.com/maps/dir/${fromPoint}/${toPoint}`;
+    return url;
+}
+
+export { distance, roundToTwoDigits, createLinkToGoogleMaps };
