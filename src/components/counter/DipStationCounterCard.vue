e3d46d0a3e92b4fe3c045242340000cea1977c07
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-03-21 20:13:46 +0200

    feat: separate views for counters

---
+++ src/components/counter/DipStationCounterCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>dipstation</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'DipStationCounterCard',
+    data() {
+        return {};
+    }
+});
+</script>
---
+++ src/components/counter/PullUpCounterCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>pullup</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'PullUpCounterCard',
+    data() {
+        return {};
+    }
+});
+</script>
