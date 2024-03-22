4d1d3fcd8788df72e4213d2af89199c904292e7b
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-03-22 10:33:44 +0200

    feat: style cards

--- src/components/counter/PullUpCounterCard.vue
+++ src/components/counter/PullUpCounterCard.vue
@@ -1,11 +1,22 @@
 <template>
-    <div class="card q-my-md">
-        <div>{{ counter.reps }}</div>
-    </div>
+    <q-card class="card shadow q-my-md flex justify-between">
+        <q-card-section class="self-center q-ml-sm">
+            <div style="min-width:20px">{{ counter.reps }}</div>
+        </q-card-section>
+        <q-card-section class="q-ml-sm q-py-sm">
+            <div class="text-center">{{ renderDate(counter.created_at) }}</div>
+            <div class="text-center">{{ renderTime(counter.created_at) }}</div>
+        </q-card-section>
+        <q-card-section class="self-center q-py-none">
+            <q-icon name="fa-solid fa-trash-can" class="cursor-pointer"
+                color="negative" size="sm" />
+        </q-card-section>
+    </q-card>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
+import { renderDate, renderTime } from 'src/utils/datetime'
 
 export default defineComponent({
     name: 'PullUpCounterCard',
@@ -15,16 +26,19 @@ export default defineComponent({
             required: true
         },
     },
-    data() {
-        return {};
-    }
+    setup() {
+        return {
+            renderDate,
+            renderTime,
+        }
+    },
 });
 </script>
 
 <style scoped>
 .card {
     min-height: 50px;
+    max-height: 70px;
     min-width: 314px;
-    box-shadow: rgba(0, 0, 0, 0.09) 0px 3px 12px;
 }
 </style>
