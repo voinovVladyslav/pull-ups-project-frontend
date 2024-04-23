80ede3caa7bdfb8ee6a14c5f6d4abefde44f5632
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-04-23 18:22:14 +0300

    feat: configure apex charts + demo chart

--- src/pages/ChartsPage.vue
+++ src/pages/ChartsPage.vue
@@ -1,15 +1,57 @@
 <template>
     <q-page class="q-pa-md">
-        charts
+        <h1>Charts</h1>
+        <apexchart type="line" height="350" :options="chartOptions"
+            :series="series"></apexchart>
     </q-page>
 </template>
 
 
 <script>
 import { defineComponent } from 'vue';
+import VueApexCharts from 'vue3-apexcharts'
 
 export default defineComponent({
     name: "ChartsPage",
+    components: {
+        apexchart: VueApexCharts,
+    },
+    data() {
+        return {
+            series: [{
+                name: "Pull Ups",
+                data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+            }],
+            chartOptions: {
+                chart: {
+                    height: 350,
+                    type: 'line',
+                    zoom: {
+                        enabled: false
+                    }
+                },
+                dataLabels: {
+                    enabled: true
+                },
+                stroke: {
+                    curve: 'straight'
+                },
+                title: {
+                    text: 'Product Trends by Month',
+                    align: 'left'
+                },
+                grid: {
+                    row: {
+                        colors: ['#f3f3f3', 'transparent'],
+                        opacity: 0.5
+                    },
+                },
+            },
+
+        }
+
+
+    },
 })
 
 </script>
